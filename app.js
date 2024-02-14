abfb4cff23996f29fd9085c656d17363f6c82aba
Shivendra490 <shivendrasrivastava490@gmail.com>
2024-02-14 19:48:49 +0530

    ejs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes=require('./routes/shop')
 
 const app=express()
 
-app.set('view engine','pug')
+app.set('view engine','ejs')
 app.set('views','views')
 
 const arr=['arr1','arr2','arr3','arr4']
