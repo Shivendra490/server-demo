cb0ccc7a587d8c00d759961ccf257afbb67d6a3d
Shivendra490 <shivendrasrivastava490@gmail.com>
2024-02-17 12:35:15 +0530

    additional link product detail

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,6 +18,15 @@ exports.getProducts=(req,res,next)=>{
 }
 
 
+exports.getProduct=(req,res,next)=>{
+  const prodId=req.params.productId
+    Product.findById(prodId,product=>{
+        console.log(product)
+        res.render('shop/product-details',{product:product,path:"/details",pageTitle:product.title,path:"/products"})
+    })
+    
+}
+
 
 
 exports.getCart=(req,res,next)=>{
--- models/product.js
+++ models/product.js
@@ -26,7 +26,7 @@ module.exports=class Product{
     }
 
     save(){
-
+        this.id=Math.random().toString()
         getProductsFromFile((products)=>{
             products.push(this)   
             fs.writeFile(p,JSON.stringify(products),(err)=>{
@@ -52,6 +52,12 @@ module.exports=class Product{
         
         getProductsFromFile(cb)
        
-       
+    }
+
+    static findById(id,cb){
+        getProductsFromFile((products)=>{
+            const product=products.find((prd)=>prd.id===id)
+            cb(product)
+        })
     }
 }
